cb2ff3f093136d65c81126cb5aa10f491f82306d
ryanjlee <ryanjlee12@gmail.com>
2014-12-29 17:46:03 -0800

    Display existing messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,19 @@
-// YOUR CODE HERE:
+/* globals $,_ */
 
-// var $messages = $.get('https://api.parse.com/1/classes/chatterbox').responseJSON.results;
-// console.log()
+var messages;
+
+$.get('https://api.parse.com/1/classes/chatterbox', function(data) {
+  messages = data.results;
+  console.log(messages);
+  _.each(messages, function(object) {
+    var string = '<li>' + object.createdAt + '://////      ' + object.username + ': ' + object.text + '</li>';
+    $('#main').append(string);
+  });
+});
+
+// $.ajax({
+//   url: 'https://api.parse.com/1/classes/chatterbox',
+//   type: 'GET',
+//   dataType: 'json',
+//   success: console.log("success")
+// });
