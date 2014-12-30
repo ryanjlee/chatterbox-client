597cf23235f2766e4c13e8d943a288fcdacb7b4c
ryanjlee <ryanjlee12@gmail.com>
2014-12-30 12:32:01 -0800

    Add room list

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,10 +3,15 @@
 var app = {};
 app.init = function() {};
 app.send = function(message) {
+  var newMessage = {
+    'username': window.location.search.substring(10),
+    'text': message,
+    'roomname': '<script>background-color: red</script>'
+  };
   $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'POST',
-    data: JSON.stringify(message),
+    data: JSON.stringify(newMessage),
     contentType: 'application/json',
     success: function (data){
       console.log('chatterbox: Message sent');
@@ -26,13 +31,14 @@ app.checkHacks = function(value){
 };
 
 app.fetch = function() {
-  $("li").remove();
 
   $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+    //url: 'https://api.parse.com/1/classes/chatterbox?where={"username":"yolo"}?order=createdAt',
     type: 'GET',
     dataType: 'json',
     success: function(data) {
+      $('#main li').remove();
       var messages = data.results;
       _.each(messages, function(object) {
         var name = app.checkHacks(object.username);
@@ -56,13 +62,53 @@ app.addMessage = function() {};
 //   });
 // });
 
-var newMessage = {
-  'username': 'MC Hammer',
-  'text': 'Can\'t touch this',
-  'roomname': 'backAlley'
-};
 //app.send(newMessage);
 app.fetch();
 
 
-// setInterval(app.fetch,5000);
+//setInterval(app.fetch,3000);
+
+// $(document).ready(function() {
+//   $('button').on('click', function() {
+//   alert('hello');
+//   });
+// });
+
+
+
+$(document).ready(function() {
+  $('#target').submit(function(event) {
+    if ($('input:first').val()) {
+      app.send($('input:first').val());
+      $('input:first').val('');
+    }
+    event.preventDefault();
+  });
+});
+
+
+app.getRooms = function(){
+  var rooms =  {};
+
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      var messages = data.results;
+      _.each(messages, function(object) {
+        var room = app.checkHacks(object.roomname);
+        rooms[room] = room;
+      });
+
+      for(var key in rooms){
+        var string = '<p>'+ key + '</p>';
+        $('#rooms').append(string);
+      }
+
+    }
+  });
+  return rooms;
+};
+
+app.getRooms();
